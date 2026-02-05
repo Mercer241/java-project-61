bb4ef396852939f90757f89bc9b6ffc7bb4032ba
Александр Приходько <sasha-1337@mail.ru>
2026-02-06 02:35:16 +0500

    add GCD game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
+import hexlet.code.games.Gcd;
 
 import java.util.Scanner;
 
@@ -15,6 +16,7 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
@@ -32,6 +34,9 @@ public class App {
                 break;
             case 3:
                 Calc.start();
+                break;
+            case 4:
+                Gcd.start();
                 break;
             default:
                 System.out.println("Invalid choice!");
---
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -0,0 +1,36 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+import java.util.Random;
+
+public class Gcd {
+    private static final String DESCRIPTION = "Find the greatest common divisor of given numbers";
+    private static final int MAX_NUMBER = 100;
+
+    public static void start() {
+        String[][] roundsData = new String[Engine.ROUNDS_COUNT][2];
+        Random random = new Random();
+
+        for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
+            int a = random.nextInt(MAX_NUMBER) + 1;
+            int b = random.nextInt(MAX_NUMBER) + 1;
+
+            int correctAnswer = gcd(a, b);
+            String question = a + " " + b;
+
+            roundsData[i][0] = question;
+            roundsData[i][1] = Integer.toString(correctAnswer);
+        }
+
+        Engine.run(DESCRIPTION, roundsData);
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+            return a;
+    }
+}
